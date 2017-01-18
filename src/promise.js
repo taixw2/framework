0fd943c6ac8c35605e8370fba9c35a3d57bd3e44
iqqq <254225961@qq.com>
2017-01-18 23:22:44 +0800

    =

--- src/promise.js
+++ src/promise.js
@@ -3,20 +3,91 @@ import {
   noop
 } from "./utils.js";
 
+const PENDING = "pending";
+const SEALED = "sealed";
+const FULFILLED = "fulfilled";
+const REJECTED = "rejected";
+
+let asyncQueue = [];
+let asyncTimer = false;
+
+function invokeResolver(resolver,promise) {
+
+  function resolverPromise(value) {
+
+    if (promise.state_ === PENDING) {
+      promise.state_ = SEALED;
+      promise.data_ = value;
+      asyncCall(publishFulfillment,promise);
+    }
+  }
+
+  function rejectPromise(reason) {
+
+  }
+  try {
+    resolver(resolverPromise,rejectPromise);
+  } catch (e) {
+    rejectPromise(e);
+  }
+
+}
+
+function asyncCall(callback,args) {
+  var promiseCallback;
+  asyncQueue.push([callback,args]);
+
+  if (!asyncTimer) {
+      asyncTimer = true;
+      /*jshint ignore:start*/
+      while (promiseCallback = asyncQueue.shift()) {
+        promiseCallback[0](promiseCallback[1])
+      }
+      /*jshint ignore:end*/
+      asyncQueue = [];
+      asyncTimer = false;
+  }
+}
+
+
+function publishFulfillment(promise){
+  var callbacks = promise._then;
+  var callback;
+  promise.state_ = FULFILLED;
+
+  /*jshint ignore:start*/
+  while (callback = callbacks.shift()) {
+
+  }
+  /*jshint ignore:end*/
+
+}
+
 export class Promise{
 
-  constructor () {
+  constructor (resolver) {
 
     this._then = [];
 
+
+    invokeResolver(resolver,this);
+
+
+
   }
 
-  then (onFulfillment = noop, onRejection = noop) {
+  then (fulfilled = noop, rejected = noop) {
+
+
 
   }
 
   "catch" (onRejection) {
     this.then(noop,onRejection);
+  }
+
+  toString () {
+    return "[object Promise]";
   }
 
 }
